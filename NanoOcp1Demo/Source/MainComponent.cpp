ba94894d1b7aa1407437a6b71f6efc39bec894d5
Christian Ahrens <christianahrens@me.com>
2023-04-04 19:00:22 +0200

    Added sliders and buttons for gain and mute for four channels

--- NanoOcp1Demo/Source/MainComponent.cpp
+++ NanoOcp1Demo/Source/MainComponent.cpp
@@ -25,71 +25,228 @@ MainComponent::MainComponent()
     m_nanoOcp1Client = std::make_unique<NanoOcp1::NanoOcp1Client>(address, port);
 	m_nanoOcp1Client->start();
 
-    m_powerOffD40Button = std::make_unique<TextButton>("Power Off D40");
-    m_powerOffD40Button->onClick = [=]() {
-		NanoOcp1::Ocp1Header header;
-		header.m_syncVal = 0x3b;
-		header.m_protoVers = 0x0001;
-		header.m_msgSize = 0x0000001c;
-		header.m_msgType = 0x01;
-		header.m_msgCnt = 0x0001;
-		NanoOcp1::Ocp1Request request;
-		request.m_cmdSize = 0x00000013;
-		request.m_handle = 0x00000002;
-		request.m_targetONo = 0x10000100;
-		request.m_methIdDefLev = 0x0004;
-		request.m_methIdMethIdx = 0x0002;
-		request.m_paramCnt = 0x01;
-		request.m_data.push_back(0x00);
-		request.m_data.push_back(0x00);
-
-		auto headerData = header.GetSerializedData();
-		auto requestData = request.GetSerializedData();
-		auto& msgData = headerData;
-		msgData.insert(msgData.end(), requestData.begin(), requestData.end());
-
-		m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)msgData.data(), msgData.size()));
-	};
-    addAndMakeVisible(m_powerOffD40Button.get());
-
-    m_powerOnD40Button = std::make_unique<TextButton>("Power On D40");
-    m_powerOnD40Button->onClick = [=]() {
-		NanoOcp1::Ocp1Header header;
-		header.m_syncVal = 0x3b;
-		header.m_protoVers = 0x0001;
-		header.m_msgSize = 0x0000001c;
-		header.m_msgType = 0x01;
-		header.m_msgCnt = 0x0001;
-		NanoOcp1::Ocp1Request request;
-		request.m_cmdSize = 0x00000013;
-		request.m_handle = 0x00000002;
-		request.m_targetONo = 0x10000100;
-		request.m_methIdDefLev = 0x0004;
-		request.m_methIdMethIdx = 0x0002;
-		request.m_paramCnt = 0x01;
-		request.m_data.push_back(0x00);
-		request.m_data.push_back(0x01);
-
-		auto headerData = header.GetSerializedData();
-		auto requestData = request.GetSerializedData();
-		auto& msgData = headerData;
-		msgData.insert(msgData.end(), requestData.begin(), requestData.end());
-		m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)msgData.data(), msgData.size()));
-	};
-    addAndMakeVisible(m_powerOnD40Button.get());
 	m_ipAndPortEditor = std::make_unique<TextEditor>();
 	m_ipAndPortEditor->setTextToShowWhenEmpty(address + ";" + juce::String(port), getLookAndFeel().findColour(juce::TextEditor::ColourIds::textColourId).darker().darker());
 	m_ipAndPortEditor->addListener(this);
 	addAndMakeVisible(m_ipAndPortEditor.get());
+
+	m_5DpowerOnButton = std::make_unique<TextButton>();
+	m_5DpowerOnButton->setClickingTogglesState(true);
+	m_5DpowerOnButton->setButtonText("PwrON");
+	m_5DpowerOnButton->setColour(juce::TextButton::ColourIds::buttonOnColourId, juce::Colours::forestgreen);
+	m_5DpowerOnButton->addListener(this);
+	addAndMakeVisible(m_5DpowerOnButton.get());
+
+	m_5DgainSliderCh1 = std::make_unique<Slider>();
+	m_5DgainSliderCh1->setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+	m_5DgainSliderCh1->setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 60, 15);
+	m_5DgainSliderCh1->addListener(this);
+	addAndMakeVisible(m_5DgainSliderCh1.get());
+	m_5DgainSliderCh2 = std::make_unique<Slider>();
+	m_5DgainSliderCh2->setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+	m_5DgainSliderCh2->setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 60, 15);
+	m_5DgainSliderCh2->addListener(this);
+	addAndMakeVisible(m_5DgainSliderCh2.get());
+	m_5DgainSliderCh3 = std::make_unique<Slider>();
+	m_5DgainSliderCh3->setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+	m_5DgainSliderCh3->setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 60, 15);
+	m_5DgainSliderCh3->addListener(this);
+	addAndMakeVisible(m_5DgainSliderCh3.get());
+	m_5DgainSliderCh4 = std::make_unique<Slider>();
+	m_5DgainSliderCh4->setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+	m_5DgainSliderCh4->setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 60, 15);
+	m_5DgainSliderCh4->addListener(this);
+	addAndMakeVisible(m_5DgainSliderCh4.get());
+
+	m_5DmuteButtonCh1 = std::make_unique<TextButton>();
+	m_5DmuteButtonCh1->setClickingTogglesState(true);
+	m_5DmuteButtonCh1->setButtonText("MUTE Ch1");
+	m_5DmuteButtonCh1->setColour(juce::TextButton::ColourIds::buttonOnColourId, juce::Colours::indianred);
+	m_5DmuteButtonCh1->addListener(this);
+	addAndMakeVisible(m_5DmuteButtonCh1.get());
+	m_5DmuteButtonCh2 = std::make_unique<TextButton>();
+	m_5DmuteButtonCh2->setClickingTogglesState(true);
+	m_5DmuteButtonCh2->setButtonText("MUTE Ch2");
+	m_5DmuteButtonCh2->setColour(juce::TextButton::ColourIds::buttonOnColourId, juce::Colours::red);
+	m_5DmuteButtonCh2->addListener(this);
+	addAndMakeVisible(m_5DmuteButtonCh2.get());
+	m_5DmuteButtonCh3 = std::make_unique<TextButton>();
+	m_5DmuteButtonCh3->setClickingTogglesState(true);
+	m_5DmuteButtonCh3->setButtonText("MUTE Ch3");
+	m_5DmuteButtonCh3->setColour(juce::TextButton::ColourIds::buttonOnColourId, juce::Colours::red);
+	m_5DmuteButtonCh3->addListener(this);
+	addAndMakeVisible(m_5DmuteButtonCh3.get());
+	m_5DmuteButtonCh4 = std::make_unique<TextButton>();
+	m_5DmuteButtonCh4->setClickingTogglesState(true);
+	m_5DmuteButtonCh4->setButtonText("MUTE Ch4");
+	m_5DmuteButtonCh4->setColour(juce::TextButton::ColourIds::buttonOnColourId, juce::Colours::red);
+	m_5DmuteButtonCh4->addListener(this);
+	addAndMakeVisible(m_5DmuteButtonCh4.get());
     
-	setSize(150, 150);
+	setSize(225, 300);
 }
 
 MainComponent::~MainComponent()
 {
 }
 
+const std::vector<std::uint8_t> MainComponent::CreateMute5DData(int channel, bool muteState)
 {
+	NanoOcp1::Ocp1Header header;
+	header.m_syncVal = 0x3b;
+	header.m_protoVers = 0x0001;
+	header.m_msgSize = 0x0000001c;
+	header.m_msgType = 0x01;
+	header.m_msgCnt = 0x0001;
+	NanoOcp1::Ocp1Request request;
+	request.m_cmdSize = 0x00000013;
+	request.m_handle = 0x00000002;
+	request.m_targetONo = NanoOcp1::NanoOcp1Base::GetDBONo2(2, 0, channel, 0x00, 0x01);
+	request.m_methIdDefLev = 0x0004;
+	request.m_methIdMethIdx = 0x0002;
+	request.m_paramCnt = 0x01;
+	request.m_data.push_back(0x00);
+	request.m_data.push_back(muteState ? 0x01 : 0x02);
+
+	auto headerData = header.GetSerializedData();
+	auto requestData = request.GetSerializedData();
+	auto& msgData = headerData;
+	msgData.insert(msgData.end(), requestData.begin(), requestData.end());
+
+	return msgData;
+}
+
+const std::vector<std::uint8_t> MainComponent::CreateGain5DData(int /*channel*/, double /*gainValue*/)
+{
+	NanoOcp1::Ocp1Header header;
+	//header.m_syncVal = 0x3b;
+	//header.m_protoVers = 0x0001;
+	//header.m_msgSize = 0x0000001c;
+	//header.m_msgType = 0x01;
+	//header.m_msgCnt = 0x0001;
+	//NanoOcp1::Ocp1Request request;
+	//request.m_cmdSize = 0x00000013;
+	//request.m_handle = 0x00000002;
+	//request.m_targetONo = NanoOcp1::NanoOcp1Base::GetDBONo2(2, 0, channel, NanoOcp1::NanoOcp1Base::DS100MatrixInputBox, NanoOcp1::NanoOcp1Base::DS100MatrixInputObjectNumber::MatrixInputMute);
+	//request.m_methIdDefLev = 0x0004;
+	//request.m_methIdMethIdx = 0x0002;
+	//request.m_paramCnt = 0x01;
+	//request.m_data.push_back(0x00);
+	//request.m_data.push_back(muteState ? 0x01 : 0x02);
+
+	auto headerData = header.GetSerializedData();
+	//auto requestData = request.GetSerializedData();
+	auto& msgData = headerData;
+	//msgData.insert(msgData.end(), requestData.begin(), requestData.end());
+
+	return msgData;
+}
+
+const std::vector<std::uint8_t> MainComponent::CreatePowerOn5DData(bool onState)
+{
+	NanoOcp1::Ocp1Header header;
+	header.m_syncVal = 0x3b;
+	header.m_protoVers = 0x0001;
+	header.m_msgSize = 0x0000001c;
+	header.m_msgType = 0x01;
+	header.m_msgCnt = 0x0001;
+	NanoOcp1::Ocp1Request request;
+	request.m_cmdSize = 0x00000013;
+	request.m_handle = 0x00000002;
+	request.m_targetONo = 0x10000100;
+	request.m_methIdDefLev = 0x0004;
+	request.m_methIdMethIdx = 0x0002;
+	request.m_paramCnt = 0x01;
+	request.m_data.push_back(0x00);
+	request.m_data.push_back(onState ? 0x01 : 0x00);
+
+	auto headerData = header.GetSerializedData();
+	auto requestData = request.GetSerializedData();
+	auto& msgData = headerData;
+	msgData.insert(msgData.end(), requestData.begin(), requestData.end());
+
+	return msgData;
+}
+
+void MainComponent::buttonClicked(Button* button)
+{
+	if (button == m_5DmuteButtonCh1.get())
+	{
+		auto& muteData = CreateMute5DData(1, button->getToggleState());
+		if (!m_nanoOcp1Client->isConnected())
+			DBG("client not connected");
+		else if (!m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)muteData.data(), muteData.size())))
+			DBG("sending failed");
+	}
+	else if (button == m_5DmuteButtonCh2.get())
+	{
+		auto& muteData = CreateMute5DData(2, button->getToggleState());
+		if (!m_nanoOcp1Client->isConnected())
+			DBG("client not connected");
+		else if (!m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)muteData.data(), muteData.size())))
+			DBG("sending failed");
+	}
+	else if (button == m_5DmuteButtonCh3.get())
+	{
+		auto& muteData = CreateMute5DData(3, button->getToggleState());
+		if (!m_nanoOcp1Client->isConnected())
+			DBG("client not connected");
+		else if (!m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)muteData.data(), muteData.size())))
+			DBG("sending failed");
+	}
+	else if (button == m_5DmuteButtonCh4.get())
+	{
+		auto& muteData = CreateMute5DData(4, button->getToggleState());
+		if (!m_nanoOcp1Client->isConnected())
+			DBG("client not connected");
+		else if (!m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)muteData.data(), muteData.size())))
+			DBG("sending failed");
+	}
+	else if (button == m_5DpowerOnButton.get())
+	{
+		auto& pwrOnData = CreatePowerOn5DData(button->getToggleState());
+		if (!m_nanoOcp1Client->isConnected())
+			DBG("client not connected");
+		else if (!m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)pwrOnData.data(), pwrOnData.size())))
+			DBG("sending failed");
+	}
+}
+
+void MainComponent::sliderValueChanged(Slider* slider)
+{
+	if (slider == m_5DgainSliderCh1.get())
+	{
+		auto& gainData = CreateGain5DData(1, slider->getValue());
+		if (!m_nanoOcp1Client->isConnected())
+			DBG("client not connected");
+		else if (!m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)gainData.data(), gainData.size())))
+			DBG("sending failed");
+	}
+	else if (slider == m_5DgainSliderCh2.get())
+	{
+		auto& gainData = CreateGain5DData(2, slider->getValue());
+		if (!m_nanoOcp1Client->isConnected())
+			DBG("client not connected");
+		else if (!m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)gainData.data(), gainData.size())))
+			DBG("sending failed");
+	}
+	else if (slider == m_5DgainSliderCh3.get())
+	{
+		auto& gainData = CreateGain5DData(3, slider->getValue());
+		if (!m_nanoOcp1Client->isConnected())
+			DBG("client not connected");
+		else if (!m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)gainData.data(), gainData.size())))
+			DBG("sending failed");
+	}
+	else if (slider == m_5DgainSliderCh4.get())
+	{
+		auto& gainData = CreateGain5DData(4, slider->getValue());
+		if (!m_nanoOcp1Client->isConnected())
+			DBG("client not connected");
+		else if (!m_nanoOcp1Client->sendData(juce::MemoryBlock((const char*)gainData.data(), gainData.size())))
+			DBG("sending failed");
+	}
+}
 
 void MainComponent::textEditorReturnKeyPressed(TextEditor& editor)
 {
@@ -107,20 +264,29 @@ void MainComponent::textEditorReturnKeyPressed(TextEditor& editor)
 
 void MainComponent::resized()
 {
-    auto button1Bounds = getLocalBounds();
-    auto button2Bounds = button1Bounds.removeFromBottom(button1Bounds.getHeight() / 2);
 	auto connectionParamsHeight = 35;
+	auto buttonHeight = 50;
+	auto channelWidth = getWidth() / 4;
 
 	auto bounds = getLocalBounds();
 
 	auto textEditorBounds = bounds.removeFromTop(connectionParamsHeight).reduced(5);
 	m_ipAndPortEditor->setBounds(textEditorBounds);
 
-    button1Bounds.reduce(5, 5);
-    button2Bounds.reduce(5, 5);
+	auto pwrOnBounds = bounds.removeFromTop(buttonHeight).reduced(5);
+	m_5DpowerOnButton->setBounds(pwrOnBounds);
 
-    m_powerOffD40Button->setBounds(button1Bounds);
-    m_powerOnD40Button->setBounds(button2Bounds);
+	auto muteBounds = bounds.removeFromBottom(buttonHeight);
+	m_5DmuteButtonCh1->setBounds(muteBounds.removeFromLeft(channelWidth).reduced(5));
+	m_5DmuteButtonCh2->setBounds(muteBounds.removeFromLeft(channelWidth).reduced(5));
+	m_5DmuteButtonCh3->setBounds(muteBounds.removeFromLeft(channelWidth).reduced(5));
+	m_5DmuteButtonCh4->setBounds(muteBounds.reduced(5));
+
+	auto gainBounds = bounds;
+	m_5DgainSliderCh1->setBounds(gainBounds.removeFromLeft(channelWidth).reduced(5));
+	m_5DgainSliderCh2->setBounds(gainBounds.removeFromLeft(channelWidth).reduced(5));
+	m_5DgainSliderCh3->setBounds(gainBounds.removeFromLeft(channelWidth).reduced(5));
+	m_5DgainSliderCh4->setBounds(gainBounds.reduced(5));
 }
 
 }
--- NanoOcp1Demo/Source/MainComponent.h
+++ NanoOcp1Demo/Source/MainComponent.h
@@ -27,23 +27,46 @@ namespace NanoOcp1Demo
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent   : public Component
+class MainComponent   : public Component, juce::Button::Listener, juce::Slider::Listener, juce::TextEditor::Listener
 {
 public:
     //==============================================================================
     MainComponent();
     ~MainComponent();
 
+    const std::vector<std::uint8_t> CreateMute5DData(int channel, bool muteState = true);
+    const std::vector<std::uint8_t> CreateGain5DData(int channel, double gainValue = 0.0);
+    const std::vector<std::uint8_t> CreatePowerOn5DData(bool onState = true);
+
     //==============================================================================
-    void paint (Graphics&) override;
     void resized() override;
+
+    //==============================================================================
+    void buttonClicked(Button* button) override;
+
+    //==============================================================================
+    void sliderValueChanged(Slider* slider) override;
+
+    //==============================================================================
+    void textEditorReturnKeyPressed(TextEditor& editor) override;
     
 private:
-    std::unique_ptr<NanoOcp1::NanoOcp1Client>           m_nanoOcp1Client;
+    std::unique_ptr<NanoOcp1::NanoOcp1Client>  m_nanoOcp1Client;
     
     //==============================================================================
-    std::unique_ptr<TextButton>             m_powerOnD40Button;
-    std::unique_ptr<TextButton>             m_powerOffD40Button;
+    std::unique_ptr<TextEditor> m_ipAndPortEditor;
+
+    std::unique_ptr<TextButton> m_5DpowerOnButton;
+
+    std::unique_ptr<Slider>     m_5DgainSliderCh1;
+    std::unique_ptr<Slider>     m_5DgainSliderCh2;
+    std::unique_ptr<Slider>     m_5DgainSliderCh3;
+    std::unique_ptr<Slider>     m_5DgainSliderCh4;
+
+    std::unique_ptr<TextButton> m_5DmuteButtonCh1;
+    std::unique_ptr<TextButton> m_5DmuteButtonCh2;
+    std::unique_ptr<TextButton> m_5DmuteButtonCh3;
+    std::unique_ptr<TextButton> m_5DmuteButtonCh4;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
