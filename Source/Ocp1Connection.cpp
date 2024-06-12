69410745c995c67794a7841dc7b35c8bbbcbf23f
Paul Schuladen <paul.schuladen@dbaudio.com>
2024-06-12 18:30:58 +0200

    Changed Ocp1Connection::disconnect network thread exit process
    - calling thread->stopThread(timeoutMs) bring in place the thread-internal startstop lock
    - if the thread is first signaled to exit followed by a stopThread command can possibly trigger a race condition where the thread could be terminated while the stopThread method is entered

--- Source/Ocp1Connection.cpp
+++ Source/Ocp1Connection.cpp
@@ -113,14 +113,15 @@ bool Ocp1Connection::connectToSocket(const String& hostName,
 
 void Ocp1Connection::disconnect(int timeoutMs, Notify notify)
 {
-    thread->signalThreadShouldExit();
-
+    //should be called before socket->close to ensure that running processes on the thread
+    //are notified that the thread is about to exit.
+    thread->stopThread(timeoutMs);
+    
     {
         const ScopedReadLock sl(socketLock);
         if (socket != nullptr)  socket->close();
     }
-
-    thread->stopThread(timeoutMs);
+    
     deleteSocket();
 
     if (notify == Notify::yes)
